3120cf5f52ba7348685e700437aff0a668d9632b
slemanz <william_sleman@hotmail.com>
2025-02-28 19:15:55 -0300

    storage(feat): add prototypes

--- shared/Inc/storage.h
+++ shared/Inc/storage.h
@@ -1,6 +1,21 @@
 #ifndef STORAGE_H_
 #define STORAGE_H_
 
+#include <stdint.h>
 
+typedef struct
+{
+    uint8_t day;
+    uint8_t month;
+    uint8_t year;
+    uint8_t hour;
+    uint8_t minute;
+}storage_date_t;
+
+
+uint8_t storage_getCount(void);
+void    storage_updateCount(uint8_t count);
+
+void    storage_addDate(storage_date_t date);
 
 #endif /* STORAGE_H_ */
