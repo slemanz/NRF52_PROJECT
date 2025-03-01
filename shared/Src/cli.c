e8a11463c9eabc6e8b0254bc7b4d7ccb8f6ffa00
slemanz <william_sleman@hotmail.com>
2025-03-01 06:24:58 -0300

    cli(feat): add command to see storage data count

--- shared/Src/cli.c
+++ shared/Src/cli.c
@@ -1,12 +1,15 @@
 #include "cli.h"
 #include "temperature.h"
+#include "storage.h"
 
 static void get_temperature(void);
+static void get_count(void);
 static void Help(void);
 
 commandStruct_t commands[] = {
-    {"GET TEMPERATURE", get_temperature, "Shows temperature value"},
-    {"HELP", Help, "List all commands"},
+    {"temperature", get_temperature, "Shows temperature value"},
+    {"count", get_count, "Shows storage data count"},
+    {"help", Help, "List all commands"},
 };
 
 #define COMMAND_COUNT           (sizeof(commands) / sizeof(commands[0]))
@@ -47,6 +50,13 @@ static void get_temperature(void)
     uint16_t temperature_value = temperature_get();
     printf("Temperatura: %d.%d C\n", temperature_value/10, temperature_value%10);
 }
+
+static void get_count(void)
+{
+    uint8_t count_data = storage_getCount();
+    printf("Count: %d\n", count_data);
+}
+
 
 static void Help(void)
 {
