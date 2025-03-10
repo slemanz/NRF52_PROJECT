00568caa22a01f79aab4a15b92a61a7a679eee51
slemanz <william_sleman@hotmail.com>
2025-03-10 10:57:21 -0300

    project(feat): begin to test another temperature ic

--- app/Src/main.c
+++ app/Src/main.c
@@ -24,8 +24,23 @@ int main(void)
     GPIO_WriteToOutputPin(GPIOP0, LED_BUILT_IN, GPIO_PIN_SET);
     for(uint32_t i = 0; i < 500000; i++) __asm("NOP"); // stable the system
     printf("Init system ok...\n\r");
+
+    uint8_t teste[10];
+
+
+    TWI_MasterReceiveData(TWI1, teste, 4, 0x44);
+    uint16_t temperature_value = (((teste[2] << 8) | (teste[3])) >> 2);
+    printf("Temperatura: %d C\n", temperature_value);
+
+    TWI_MasterReceiveData(TWI1, teste, 4, 0x44);
+    printf("%d, %d, %d, %d \n", teste[0], teste[1], teste[2], teste[3]);
+    printf("%d, %d, %X, %X \n", teste[0], teste[1], teste[2], teste[3]);
     
-    printf("Temperature %d C\n", temperature_get()/10);
+    temperature_value = (((teste[2] << 8) | (teste[3]) ) >> 2 );
+    float temperatura = ((temperature_value/16383.0)*165.0 - 40.0)*10;
+    temperature_value = (int16_t)temperatura;
+    printf("Temperatura: %d C\n", temperature_value);
+    
 
     /*
     saadc_selectInp(SAADC_PSEL_AIN2);
@@ -34,10 +49,13 @@ int main(void)
     */
 
     //storage_clean();
+    /*
     storage_updateCount();
     uint8_t count_data = storage_getCount();
     printf("Count: %d\n", count_data);
+    while(1);
     storage_temperatureAppend(temperature_get());
+    */
 
 
     while (1)
@@ -52,14 +70,17 @@ int main(void)
         if((system_get_ticks() - start_time) >= 1000)
         {
             GPIO_ToggleOutputPin(GPIOP0, LED_GREEN_PIN);
-
             start_time = system_get_ticks();
         }
         
 
         if((system_get_ticks() - start_time2) >= 120000) 
         {
-            storage_temperatureAppend(temperature_get());
+            TWI_MasterReceiveData(TWI1, teste, 4, 0x44);
+            temperatura = ((temperature_value/16383.0)*165.0 - 40.0)*10;
+            temperature_value = (int16_t)temperatura;
+            printf("Temperatura: %d.%d C\n", temperature_value/10, temperature_value%10);
+            //storage_temperatureAppend(temperature_get());
             start_time2 = system_get_ticks();
         }
     }
--- bsp/Src/temperature.c
+++ bsp/Src/temperature.c
@@ -8,12 +8,15 @@
 #include "temperature.h"
 #include "driver_twi.h"
 
+// LM75A NXP
+
 uint16_t temperature_get(void)
 {
     uint8_t temperatureAddress = 0x4F;
-    int8_t twi_read[2];
+    int8_t twi_read[2] = {0, 0};
 
-    TWI_MasterReceiveData(TWI1, 0x00, (uint8_t*)twi_read, 2, temperatureAddress);
+    //TWI_MasterReceiveData(TWI1, 0x00, (uint8_t*)twi_read, 2, temperatureAddress);
+    //uint16_t temperature_value = ((twi_read[0]*10) + ((twi_read[1] >> 7)*5));
     uint16_t temperature_value = ((twi_read[0] << 8 | twi_read[1]) >> 5) & 0x7FF;
 
     return temperature_value;
